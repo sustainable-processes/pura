87b79294bf13ece304694870afc7025ce3ba9429
Kobi Felton <kobi.c.f@gmail.com>
2022-07-15 13:49:51 +0100

    Set up balancing code

--- pura/balance.py
+++ pura/balance.py
@@ -1,62 +1,22 @@
-from typing import List, Tuple, Optional, Dict
-import pandas as pd
-from enum import Enum
-
-from pydantic import BaseModel
+from typing import List, Tuple
+from .reaction import ReactionIdentifier, ReactionIdentifierType
 
 
-class ReactionIdentifierType:
-    UNSPECIFIED = 0
-    CUSTOM = 1
-    REACTION_SMILES = 2
-    REACTION_CXSMILES = 6
-    # Extended SMILES.
-    RDFILE = 3
-    # Reaction data file.
-    RINCHI = 4
-    # Reaction InChI.
-    NAME = 5
-    # Named reaction or reaction category.
+def balance_reactions(
+    reactions: List[ReactionIdentifier],
+    ignore_hydrogens: bool = True,
+    use_ray: bool = False,
+) -> List[Tuple[ReactionIdentifier, ReactionIdentifier]]:
+    if use_ray:
 
+        @ray.remote
+        def balance_reaction_ray(**kwargs):
+            balance_reaction(**kwargs)
 
-class ReactionIdentifier(BaseModel):
-    identifier_type: ReactionIdentifierType
-    value: str
-    details: Optional[str] = None
-
-
-class Reaction(BaseModel):
-    def __init__(
-        self, reaction_identifier: List[ReactionIdentifier], conditions: Dict
-    ) -> None:
-        pass
-
-    def to_row(self):
-        return
-
-
-def to_frame(
-    reactions: List[Reaction], columns: List[str], max_rows: int = 2
-) -> pd.DataFrame:
-    reaction_dicts = [reaction.to_row() for reaction in reactions]
-    return pd.DataFrame(reaction_dicts)
-
-
-# def balance_reactions(
-#     reactions: List[ReactionIdentifier],
-#     ignore_hydrogens: bool = True,
-#     use_ray: bool = False,
-# ) -> List[Tuple[ReactionIdentifier, ReactionIdentifier]]:
-#     if use_ray:
-#         for reaction in reactions:
-#             ray.remote(balance_reaction_ray)
-#     else:
-#         [balance_reaction(reaction) for reaction in reactions]
-
-
-# @ray.remote
-# def balance_reaction_ray(**kwargs):
-#     balance_reaction(**kwargs)
+        for reaction in reactions:
+            ray.remote(balance_reaction_ray)
+    else:
+        [balance_reaction(reaction) for reaction in reactions]
 
 
 def balance_reaction(
---
+++ pura/reaction.py
@@ -0,0 +1,40 @@
+from typing import List, Tuple, Optional, Dict
+import pandas as pd
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class ReactionIdentifierType:
+    UNSPECIFIED = 0
+    CUSTOM = 1
+    REACTION_SMILES = 2
+    REACTION_CXSMILES = 6
+    # Extended SMILES.
+    RDFILE = 3
+    # Reaction data file.
+    RINCHI = 4
+    # Reaction InChI.
+    NAME = 5
+    # Named reaction or reaction category.
+
+
+class ReactionIdentifier(BaseModel):
+    identifier_type: ReactionIdentifierType
+    value: str
+    details: Optional[str] = None
+
+
+class Reaction(BaseModel):
+    def __init__(
+        self, reaction_identifier: List[ReactionIdentifier], conditions: Dict
+    ) -> None:
+        pass
+
+    def to_row(self):
+        return
+
+
+def to_frame(reactions: List[Reaction], columns: List[str]) -> pd.DataFrame:
+    reaction_dicts = [reaction.to_row() for reaction in reactions]
+    return pd.DataFrame(reaction_dicts)
